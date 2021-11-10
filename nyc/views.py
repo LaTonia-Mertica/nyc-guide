11fec1cd8cd00eb336e2752460b51aad0af38b05
La'Tonia Mertica <latoniamertica@gmail.com>
2021-11-10 16:31:54 -0500

    group effort via zoom to finalize - fully functional with basic styles

--- nyc/views.py
+++ nyc/views.py
@@ -12,9 +12,8 @@ def borough(request, borough):
 # Create your views here.
 def activity(request, borough, activity):
     if request.method == 'GET':
-        return render(request=request, template_name='activity.html', context={ 'activity': activity, 'activity': boroughs[borough][activity].keys() })
+        return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'activities': boroughs[borough][activity].keys() })
 
 def venue(request, borough, activity, venue):
     if request.method == 'GET':
-        return render(request=request, template_name='venue.html', context={ 'activity': activity, 'venue': boroughs[borough][activity][venue].keys() })
-  
+        return render(request=request, template_name='venue.html', context={ 'borough': borough, 'activity': activity, 'venue': venue, 'description': boroughs[borough][activity][venue].get('description', 'img_link') })
